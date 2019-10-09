6dcb5ffe4a3476857fc6016b326852605a626136
Krystian <krystian_konecki@wp.pl>
2019-10-09 15:47:23 +0200

    Updates

---
+++ precache-manifest.d693cd40e1b48d4c0391e612bdb9acb3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0f66ee6b966719198b36df053b9009c",
+    "url": "/To-Do-List-React/index.html"
+  },
+  {
+    "revision": "e27d9ba413c7eded9b80",
+    "url": "/To-Do-List-React/static/css/main.edbc8b49.chunk.css"
+  },
+  {
+    "revision": "652b7671340c6bf7749d",
+    "url": "/To-Do-List-React/static/js/2.8a089c29.chunk.js"
+  },
+  {
+    "revision": "e27d9ba413c7eded9b80",
+    "url": "/To-Do-List-React/static/js/main.fab83cc1.chunk.js"
+  },
+  {
+    "revision": "bb737b9415a649ec5314",
+    "url": "/To-Do-List-React/static/js/runtime-main.9b4ac536.js"
+  }
+]);
